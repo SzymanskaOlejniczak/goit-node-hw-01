920b434888db57f29fb6eac90216a8f065ab2279
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-03-05 20:17:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 const contacts = require("./contacts");
-
 const { Command } = require("commander");
+
 const program = new Command();
 program
-	.option("-a, --action <type>", "choose action")
-	.option("-i, --id <type>", "user id")
-	.option("-n, --name <type>", "user name")
-	.option("-e, --email <type>", "user email")
-	.option("-p, --phone <type>", "user phone");
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
-	switch (action) {
-		case "list":
-			contacts.listContacts();
-			break;
+  switch (action) {
+    case "list":
+      contacts.listContacts();
+      break;
 
-		case "get":
-			contacts.getContactById(id);
-			break;
+    case "get":
+      contacts.getContactById(id);
+      break;
 
-		case "add":
-			contacts.addContact(name, email, phone);
-			break;
+    case "add":
+      contacts.addContact(name, email, phone);
+      break;
 
-		case "remove":
-			contacts.removeContact(id);
-			break;
+    case "remove":
+      contacts.removeContact(id);
+      break;
 
-		default:
-			console.warn("\x1B[31m Unknown action type!");
-	}
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
 }
 
 invokeAction(argv);
